688b745cdeac4bca08bbbcd1f0a3e0ba65ac88f8
padredemellis <10656@holbertonstudents.com>
2026-01-26 13:29:56 -0300

    more_dart

---
+++ 2_Final_or_Const.dart
@@ -0,0 +1,5 @@
+void main() {
+  final currentYear = 2025;
+  const pi = 3.14159;
+  print("currentYear: $currentYear \npi: $pi");
+}
---
+++ 3_Type_Inference_and_Explicit_Types.dart
@@ -0,0 +1,6 @@
+void main() {
+  var number = 42;
+  int num = 7;
+  print("The type of ${number} is: ${number.runtimeType}");
+  print("The type of ${num} is: ${number.runtimeType}");
+}
