ca09110629f0c3e160a871e107f0dfd92859c1c6
padredemellis <10656@holbertonstudents.com>
2026-01-26 13:44:03 -0300

    more_dart

---
+++ 5_Operators_Playground.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 10;
+  int b = 3;
+  print("a + b = ${a + b}");
+  print("a - b = ${a - b}");
+  print("a * b = ${a * b}");
+  print("a / b = ${a / b}");
+  print("a > b = ${a > b}");
+  print("a = b = ${a == b}");
+  print("a + b = ${a + b}");
+  print("a > 5 && b < 5 = ${a > 5 && b < 5}");
+  print("a < 5 || b < 5 = ${a < 5 || b < 5}");
+
+}
