67fd98a094067979ea56f653f0ef0eff6e36189e
padredemellis <10656@holbertonstudents.com>
2026-01-26 14:22:32 -0300

    more_dart

---
+++ weekday_Name_using_switch.dart
@@ -0,0 +1,30 @@
+void main() {
+  int dayNumber = 3;
+
+  switch (dayNumber) {
+    case 1:
+      print("Weekday: Monday");
+      break;
+    case 2:
+      print("Weekday: Tuesday");
+      break;
+    case 3:
+      print("Weekday: Wednesday");
+      break;
+    case 4:
+      print("Weekday: Thursday");
+      break;
+    case 5:
+      print("Weekday: Friday");
+      break;
+    case 6:
+      print("Weekday: Saturday");
+      break;
+    case 7:
+      print("Weekday: Sunday");
+      break;
+    default:
+      print("Invalid day");
+      break;
+  }
+}
